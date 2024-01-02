99de72f3d0732ea9e4e39630d8ac49701cc6e001
Karanbohara01 <karanbohara216@gmail.com>
2024-01-02 18:41:43 +0545

    header-component-created

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Layout from "./components/layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Shop from "./pages/shop";
+import Blog from "./pages/blog";
+import About from "./pages/about";
+import Contact from "./pages/contact";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="shop" element={<Shop />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { BiPhoneCall } from "react-icons/bi";
 import { IoIosMail } from "react-icons/io";
 import { Link } from "react-router-dom";
+import logo from "../assets/images/apneck.png";
+import { HiOutlineInboxIn } from "react-icons/hi";
+import { VscAccount } from "react-icons/vsc";
+import { MdShoppingBasket } from "react-icons/md";
 
 const Header = () => {
   return (
     <>
-      <header className="header-top-strip p-1 shadow-sm">
+      <header className="header-top-strip px-4 p-1 shadow-md">
         <div className="container-xxl">
           <div className="row align-items-center">
             <div className="col-6">
@@ -24,6 +28,55 @@ const Header = () => {
                   <IoIosMail className="fs-3 mx-4" />
                 </Link>
               </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-upper px-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-2 text-center">
+              <Link to="/">
+                <img src={logo} alt="" className="img-fluid logo" />
+              </Link>
+            </div>
+            <div className="col-3 d-flex align-items-center">
+              <div className="input-group">
+                <span className="input-group-text" id="basic-addon1">
+                  All
+                </span>
+                <input
+                  type="text"
+                  class="form-control p-2"
+                  placeholder="Search products"
+                  aria-label="Recipient's username"
+                  aria-describedby="basic-addon2"
+                />
+                <button class="input-group-text" id="basic-addon2">
+                  Search
+                </button>
+              </div>
+            </div>
+            <div className=" nav-links col-4 d-flex align-items-center justify-content-between text-center m-auto">
+              <Link to={"/"}>Home</Link>
+              <Link to={"shop"}>Shop</Link>
+              <Link to={"blog"}>Blog</Link>
+              <Link to={"contact"}>Contact</Link>
+              <Link to={"about"}>About</Link>
+            </div>
+            <div className=" nav-links-nav col-3 d-flex align-items-center justify-content-around">
+              <Link className="d-flex">
+                <HiOutlineInboxIn className="fs-4 mx-2" />
+                <span>Wishlist</span>
+              </Link>
+              <Link className="d-flex">
+                <VscAccount className="fs-4 mx-2" />
+                <span>Account</span>
+              </Link>
+              <Link className="d-flex">
+                <MdShoppingBasket className="fs-4 mx-2" />
+                <span>Cart</span>
+              </Link>
             </div>
           </div>
         </div>

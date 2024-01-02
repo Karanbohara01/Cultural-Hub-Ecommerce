d096ee59457dd787225e7034d452f4d8da61390a
Karanbohara01 <karanbohara216@gmail.com>
2024-01-02 21:33:11 +0545

    footerstarted

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const footer = () => {
-  return <div>footer</div>;
+  return (
+    <>
+      <footer className="footer px-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4 d-flex flex-column">
+              <h3 className="mb-5">Contact us</h3>
+              <div className="footer-details">
+                <p>
+                  <b>Address:</b>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default footer;
